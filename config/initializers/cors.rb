ae8f657c8577c1279f9971f033919748b2dc7b9d
Nazarenko Viktor <victorn.ua@gmail.com>
2024-05-30 20:30:54 +0200

    allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000",
-            "http://127.0.0.1:3000",
-            "https://sm.orthanc.ai"
+    # origins "http://localhost:3000",
+    #         "http://127.0.0.1:3000",
+    #         "https://sm.orthanc.ai"
+    origins "*"
 
     resource "*",
       headers: :any,
